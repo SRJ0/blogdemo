db58272fd9373e1c4c813020448bb1dabad09521
malgnjosh <josh@malgnsoft.com>
2024-08-14 08:52:55 +0900

    feat: 회원 서비스

---
+++ src/main/java/com/example/blogdemo/controller/UserApiController.java
@@ -0,0 +1,32 @@
+package com.example.blogdemo.controller;
+
+import com.example.blogdemo.Service.UserDetailService;
+import com.example.blogdemo.Service.UserService;
+import com.example.blogdemo.dto.AddUserRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+    private final UserService userService;
+    private final UserDetailService userDetailService;
+
+    @PostMapping("/logout")
+    public String signup(AddUserRequest request) {
+        userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
+    }
+}
